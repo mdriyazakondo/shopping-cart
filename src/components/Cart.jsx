c8e3eef6584b584ec10d8c01558aff98b0d6d7bf
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-09-21 00:34:32 +0600

    remove console

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const Cart = ({ cart, handleRemove }) => {
-  console.log(cart);
   return (
     <div className="max-w-7xl mx-auto space-y-4 mb-10 mt-6 grid grid-cols-2">
-      {cart.map((carts) => (
-        <div className="flex gap-2 items-center ">
+      {cart.map((carts, index) => (
+        <div key={index} className="flex gap-2 items-center ">
           <img className="h-[100px]" src={carts.img} alt="" />
           <h3 className="w-64">{carts.name}</h3>
           <p className="w-32">Price: ${carts.price}</p>
